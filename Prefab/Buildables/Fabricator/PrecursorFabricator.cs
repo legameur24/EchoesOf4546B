ff169d9e6d0c807cf1f6ca9c57e8f7916d42d75b
Yansox <134643086+legameur24@users.noreply.github.com>
2025-02-17 18:49:55 +0100

    The 0.2

---
+++ Prefab/Buildables/Fabricator/PrecursorFabricator.cs
@@ -0,0 +1,45 @@
+﻿using BepInEx;
+using EchoesOf4546B.Prefab.Items.Equipment;
+using EchoesOf4546B.Prefab.Items.Materials;
+using Nautilus.Assets;
+using Nautilus.Assets.Gadgets;
+using Nautilus.Assets.PrefabTemplates;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using Color = UnityEngine.Color;
+
+namespace EchoesOf4546B.Prefab.Buildables.Fabricator
+{
+    [BepInPlugin("com.snmodding.nautilus.customfabricator", "Nautilus Custom Fabricator Example Mod", PluginInfo.PLUGIN_VERSION)]
+    [BepInDependency("com.snmodding.nautilus")]
+    public class PrecursorFabricator : BaseUnityPlugin
+    {
+        private void Awake()
+        {
+            CustomPrefab customFab = new("PrecursorFab", "Precursor Fabricator", "A precursor fabricator to craft very high technology!", SpriteManager.Get(TechType.PrecursorDroid));
+
+            customFab.CreateFabricator(out CraftTree.Type treeType)
+            .AddCraftNode(AlienAlloy.Info.TechType)
+            .AddCraftNode(TechType.PrecursorKey_Purple)
+            .AddCraftNode(TechType.PrecursorKey_Blue)
+            .AddCraftNode(TechType.PrecursorKey_Orange);
+
+            FabricatorTemplate fabPrefab = new(customFab.Info, treeType)
+            {
+                FabricatorModel = FabricatorTemplate.Model.Workbench,
+
+                ModifyPrefab = prefab => prefab.GetComponentsInChildren<Renderer>().ForEach(r => r.materials.ForEach(m => m.color = Color.black))
+
+            };
+            customFab.SetGameObject(fabPrefab);
+            customFab.Register();
+        }
+
+    }
+}
---
+++ Utils/AssetBundleLoadingUtils.cs
@@ -0,0 +1,17 @@
+﻿using System.IO;
+using System.Reflection;
+using UnityEngine;
+
+namespace EchoesOf4546B;
+
+public static class AssetBundleLoadingUtils
+{
+    public static AssetBundle LoadFromAssetsFolder(Assembly modAssembly, string assetBundleFileName)
+    {
+        return AssetBundle.LoadFromFile(Path.Combine(Path.GetDirectoryName(modAssembly.Location), "Assets", assetBundleFileName));
+    }
+    public static AssetBundle LoadFromModFolder(Assembly modAssembly, string pathToBundle)
+    {
+        return AssetBundle.LoadFromFile(Path.Combine(Path.GetDirectoryName(modAssembly.Location), pathToBundle));
+    }
+}

5c431618352baa51d09638e6c52108805261a09a
Yansox <134643086+legameur24@users.noreply.github.com>
2025-02-18 14:20:31 +0100

    0.2.0.1

---
+++ Utils/Handler/PDAHandler.cs
@@ -0,0 +1,275 @@
+﻿using BepInEx.Logging;
+using FMOD;
+using Nautilus.FMod.Interfaces;
+using Nautilus.Patchers;
+using Nautilus.Utility;
+using NLog.Common;
+using UnityEngine;
+
+namespace Nautilus.Handlers;
+
+/// <summary>
+/// A handler class for various PDA-related purposes:
+/// <list type="bullet">
+/// <item>Registering log entries.</item>
+/// <item>Registering encyclopedia (databank) entries.</item>
+/// <item>Defining data for scanning fragments and other items.</item>
+/// </list>
+/// </summary>
+public static class PDAHandler
+{
+    /// <summary>
+    /// Sound asset used for unlocking most PDA entries, which is a short but pleasant sound. Path is '<c>event:/tools/scanner/new_encyclopediea</c>'.
+    /// </summary>
+    public static FMODAsset UnlockBasic { get; } = AudioUtils.GetFmodAsset("event:/tools/scanner/new_encyclopediea");
+
+    /// <summary>
+    /// <para>Subnautica:<br/>Sound asset for unlocking important PDA entries, where PDA says "Integrating new PDA data." Path is '<c>event:/loot/new_PDA_data</c>'.</para>
+    /// <para>Below Zero:<br/>Sound asset for unlocking more important (generally story related) PDA entries. Path is '<c>event:/bz/ui/story_unlocked</c>'.</para>
+    /// </summary>
+
+    public static FMODAsset UnlockImportant { get; } = AudioUtils.GetFmodAsset("event:/loot/new_PDA_data");
+
+
+
+    /// <summary>
+    /// Edits how many fragments must be scanned before unlocking the techtype's blueprint.
+    /// </summary>
+    /// <param name="techType">Can be either techtype of the fragment or the crafted item.</param>
+    /// <param name="fragmentCount">The number of fragments to scan.</param>
+    public static void EditFragmentsToScan(TechType techType, int fragmentCount)
+    {
+        if (fragmentCount <= 0)
+        {
+            fragmentCount = 1;
+        }
+
+        PDAPatcher.FragmentCount[techType] = fragmentCount;
+
+        if (uGUI.isMainLevel)
+            PDAPatcher.InitializePostfix();
+    }
+
+    /// <summary>
+    /// Edits the time it takes to finish scanning a fragment.
+    /// </summary>
+    /// <param name="techType">Can be either techtype of the fragment or the crafted item.</param>
+    /// <param name="scanTime">The relative time spent on scanning. Default value is 1.</param>
+    public static void EditFragmentScanTime(TechType techType, float scanTime)
+    {
+        if (scanTime <= 0f)
+        {
+            scanTime = 1f;
+        }
+
+        PDAPatcher.FragmentScanTime[techType] = scanTime;
+
+        if (uGUI.isMainLevel)
+            PDAPatcher.InitializePostfix();
+    }
+
+    /// <summary>
+    /// Adds in a custom <see cref="PDAScanner.EntryData"/>. ***Cannot be used to Change the values of a techtype that has data already!***
+    /// </summary>
+    /// <param name="entryData">The <see cref="PDAScanner.EntryData"/> of the entry. Must be populated when passed in.</param>
+    public static void AddCustomScannerEntry(PDAScanner.EntryData entryData)
+    {
+        if (PDAPatcher.CustomEntryData.ContainsKey(entryData.key))
+        {
+            
+        }
+
+        PDAPatcher.CustomEntryData[entryData.key] = entryData;
+
+        if (uGUI.isMainLevel)
+            PDAPatcher.InitializePostfix();
+    }
+
+    /// <summary>
+    /// Registers a custom <see cref="PDAScanner.EntryData"/>.
+    /// </summary>
+    /// <param name="key">The scanned object's <see cref="TechType"/>. In the case of fragments, the fragment <see cref="TechType"/> is the key.</param>
+    /// <param name="blueprint">The <paramref name="blueprint"/> that is unlocked when <paramref name="key"/> is scanned. In case of fragments, this is the actual <see cref="TechType"/> that unlocks when all fragments are scanned. Can be <see cref="TechType.None"/>.</param>
+    /// <param name="isFragment">Whether the <paramref name="key"/> is a fragment or not.</param>
+    /// <param name="totalFragmentsRequired">The total amount of objects of <paramref name="key"/> that need to be scanned to unlock the <paramref name="blueprint"/> and <paramref name="encyclopediaKey"/>.</param>
+    /// <param name="scanTime">The amount of time in seconds it takes to scan this object.</param>
+    /// <param name="destroyAfterScan">Whether the object should be destroyed after the scan is finished.</param>
+    /// <param name="encyclopediaKey">The key of the encyclopedia entry that is unlocked when all the fragments are scanned.</param>
+    public static void AddCustomScannerEntry(TechType key, TechType blueprint, bool isFragment, int totalFragmentsRequired, float scanTime = 2f, bool destroyAfterScan = true, string encyclopediaKey = null)
+    {
+        AddCustomScannerEntry(new PDAScanner.EntryData()
+        {
+            key = key,
+            blueprint = blueprint,
+            isFragment = isFragment,
+            totalFragments = totalFragmentsRequired,
+            scanTime = scanTime,
+            destroyAfterScan = destroyAfterScan,
+            encyclopedia = encyclopediaKey ?? string.Empty
+        });
+    }
+
+    /// <summary>
+    /// Registers a custom <see cref="PDAScanner.EntryData"/>. This simplified overload is for prefabs that do not have fragments.
+    /// </summary>
+    /// <param name="key">The scanned object's <see cref="TechType"/>. In the case of fragments, the fragment <see cref="TechType"/> is the key.</param>
+    /// <param name="scanTime">The amount of time in seconds it takes to scan this object.</param>
+    /// <param name="destroyAfterScan">Whether the object should be destroyed after the scan is finished.</param>
+    /// <param name="encyclopediaKey">The key of the encyclopedia entry that is unlocked when <paramref name="key"/> is scanned.</param>
+    public static void AddCustomScannerEntry(TechType key, float scanTime = 2f, bool destroyAfterScan = false, string encyclopediaKey = null)
+    {
+        AddCustomScannerEntry(key, TechType.None, false, 1, scanTime, destroyAfterScan, encyclopediaKey);
+    }
+
+    /// <summary>
+    /// Adds a custom PDA log entry message. Can be played by the Story Goal system (see <see cref="StoryGoalHandler"/>) or manually through <see cref="PDALog.Add"/>.
+    /// </summary>
+    /// <param name="key">The key (unique identifier) for this entry.</param>
+    /// <param name="languageKey">The subtitles language key for this entry. Also see: <see cref="LanguageHandler.SetLanguageLine(string, string, string)"/>.</param>
+    /// <param name="sound"><para>The sound that will be played once this log entry is unlocked or played through the PDA's Log tab.</para>
+    /// <para>Note that the SoundQueue system (which plays the PDA sounds) accesses the FMODAsset's "id" field rather than the path, so make sure it is assigned properly.</para></param>
+    /// <param name="icon">The icon that will be used in the Log tab for this entry. if unassigned, it will use the default log entry icon.</param>
+    public static void AddLogEntry(string key, string languageKey, FMODAsset sound, Sprite icon = null)
+    {
+        PDALog.EntryData entry = new()
+        {
+            key = languageKey,
+            icon = icon,
+            sound = sound
+        };
+
+        PDALogPatcher.CustomEntryData[key] = entry;
+
+        if (uGUI.isMainLevel)
+            PDALogPatcher.InitializePostfix();
+    }
+
+    /// <summary>
+    /// Adds a custom PDA log entry message. Can be played by the Story Goal system (see <see cref="StoryGoalHandler"/>) or manually through <see cref="PDALog.Add"/>.
+    /// </summary>
+    /// <param name="key">The key (unique identifier) for this entry. Also used to create the sound asset, so make sure this string is <i>truly</i> unique!</param>
+    /// <param name="languageKey">The subtitles language key for this entry. Also see: <see cref="LanguageHandler.SetLanguageLine(string, string, string)"/>.</param>
+    /// <param name="sound">The sound that will be played once this log entry is unlocked or played through the PDA's Log tab.</param>
+    /// <param name="icon">The icon that will be used in the Log tab for this entry. if unassigned, it will use the default log entry icon.</param>
+    public static void AddLogEntry(string key, string languageKey, Sound sound, Sprite icon = null)
+    {
+        /*
+         * If there's an existing custom sound with the same key, we release the previous sound's handle.
+         * This is so we do not leave any unused objects in memory.
+         */
+        if (CustomSoundHandler.TryGetCustomSound(key, out var prevSound))
+        {
+            if (prevSound.hasHandle() && prevSound.handle != sound.handle)
+            {
+                prevSound.release();
+            }
+        }
+
+        CustomSoundHandler.RegisterCustomSound(key, sound, AudioUtils.BusPaths.PDAVoice);
+
+        AddLogEntry(key, languageKey, AudioUtils.GetFmodAsset(key, key), icon);
+    }
+
+    /// <summary>
+    /// Adds a custom PDA log entry message. Can be played by the Story Goal system (see <see cref="StoryGoalHandler"/>) or manually through <see cref="PDALog.Add"/>.
+    /// </summary>
+    /// <param name="key">The key (unique identifier) for this entry. Also used to create the sound asset, so make sure this string is <i>truly</i> unique!</param>
+    /// <param name="languageKey">The subtitles language key for this entry. Also see: <see cref="LanguageHandler.SetLanguageLine(string, string, string)"/>.</param>
+    /// <param name="audioClip">The sound that will be played once this log entry is unlocked or played through the PDA's Log tab. This is automatically converted to an FMOD sound.</param>
+    /// <param name="icon">The icon that will be used in the Log tab for this entry. if unassigned, it will use the default log entry icon.</param>
+    public static void AddLogEntry(string key, string languageKey, AudioClip audioClip, Sprite icon = null)
+    {
+        AddLogEntry(key, languageKey, CustomSoundHandler.RegisterCustomSound(key, audioClip, AudioUtils.BusPaths.PDAVoice, AudioUtils.StandardSoundModes_Stream), icon);
+    }
+
+    /// <summary>
+    /// Adds a custom PDA log entry message. Can be played by the Story Goal system (see <see cref="StoryGoalHandler"/>) or manually through <see cref="PDALog.Add"/>.
+    /// </summary>
+    /// <param name="key">The key (unique identifier) for this entry. Also used to create the sound asset, so make sure this string is <i>truly</i> unique!</param>
+    /// <param name="languageKey">The subtitles language key for this entry. Also see: <see cref="LanguageHandler.SetLanguageLine(string, string, string)"/>.</param>
+    /// <param name="soundFilePath">The file path on the disk to the sound that will be played once this log entry is unlocked or played through the PDA's Log tab.</param>
+    /// <param name="icon">The icon that will be used in the Log tab for this entry. if unassigned, it will use the default log entry icon.</param>
+    public static void AddLogEntry(string key, string languageKey, string soundFilePath, Sprite icon = null)
+    {
+        AddLogEntry(key, languageKey, CustomSoundHandler.RegisterCustomSound(key, soundFilePath, AudioUtils.BusPaths.PDAVoice, AudioUtils.StandardSoundModes_Stream), icon);
+    }
+
+    /// <summary>
+    /// Adds a custom PDA log entry message. Can be played by the Story Goal system (see <see cref="StoryGoalHandler"/>) or manually through <see cref="PDALog.Add"/>.
+    /// </summary>
+    /// <param name="key">The key (unique identifier) for this entry. Also used to create the sound asset, so make sure this string is <i>truly</i> unique!</param>
+    /// <param name="languageKey">The subtitles language key for this entry. Also see: <see cref="LanguageHandler.SetLanguageLine(string, string, string)"/>.</param>
+    /// <param name="fmodSound">The <see cref="IFModSound"/> instance that is used to create a custom sound. IFModSound instances have custom logic for playing sounds. that is accessed when this log entry is played.</param>
+    /// <param name="icon">The icon that will be used in the Log tab for this entry. if unassigned, it will use the default log entry icon.</param>
+    public static void AddLogEntry(string key, string languageKey, IFModSound fmodSound, Sprite icon = null)
+    {
+        CustomSoundHandler.RegisterCustomSound(key, fmodSound);
+        AddLogEntry(key, languageKey, AudioUtils.GetFmodAsset(key), icon);
+    }
+
+    /// <summary>
+    /// Adds custom entry.
+    /// </summary>
+    /// <param name="entry">The <see cref="PDAEncyclopedia.EntryData"/> entry.</param>
+    public static void AddEncyclopediaEntry(PDAEncyclopedia.EntryData entry)
+    {
+        if (PDAEncyclopediaPatcher.CustomEntryData.ContainsKey(entry.key))
+        {
+            
+        }
+
+        PDAEncyclopediaPatcher.CustomEntryData[entry.key] = entry;
+
+        if (PDAEncyclopedia.initialized)
+            PDAEncyclopediaPatcher.InitializePostfix();
+    }
+
+    /// <summary>
+    /// Registers a single encyclopedia entry into the game.
+    /// </summary>
+    /// <param name="key">Key (internal ID) of this PDA entry, primarily used for the language system.</param>
+    /// <param name="path"><para>Path to this entry in the databank.</para>
+    /// <para>To find examples of this string, open "Subnautica_Data\StreamingAssets\SNUnmanagedData\LanguageFiles\English.json" and search for "EncyPath".
+    /// Remember to omit the "EncyPath_" prefix from these language keys. An example of a proper value is: "Lifeforms/Fauna/Leviathans".</para>
+    /// <para>A list of all Databank paths can also be found in
+    /// <see href="https://subnauticamodding.github.io/Nautilus/tutorials/databank-entries.html#creating-an-entry-path">this section</see>
+    /// of Nautilus's documentation.</para>
+    /// </param>
+    /// <param name="title">Displayed title of the PDA entry in English. If set to null, you must implement your own translations. Language key is 'Ency_{<paramref name="key"/>}'.</param>
+    /// <param name="desc">Displayed description of the PDA entry in English. If set to null, you must implement your own translations. Language key is 'EncyDesc_{<paramref name="key"/>}'.</param>
+    /// <param name="image">Databank entry image. Can be null.</param>
+    /// <param name="popupImage">Small popup image in the notification. Can be null.</param>
+    /// <param name="unlockSound">Sound on unlock. Typical values are <see cref="UnlockBasic"/> and <see cref="UnlockImportant"/>. If unassigned, will have a default value of <see cref="UnlockBasic"/>.</param>
+    /// <param name="voiceLog">Audio player that will be displayed inside this PDA entry, typically used for voice logs. Can be null.</param>
+    public static void AddEncyclopediaEntry(string key, string path, string title, string desc, Texture2D image = null, Sprite popupImage = null, FMODAsset unlockSound = null, FMODAsset voiceLog = null)
+    {
+        if (string.IsNullOrEmpty(path))
+        {
+            InternalLogger.Error($"Attempting to add encyclopedia entry with null path for ClassId '{key}'!");
+            return;
+        }
+
+        var encyNodes = path.Split('/');
+
+        if (unlockSound == null)
+        {
+            unlockSound = UnlockBasic;
+        }
+
+        var encyEntryData = new PDAEncyclopedia.EntryData()
+        {
+            key = key,
+            nodes = encyNodes,
+            path = path,
+            image = image,
+            popup = popupImage,
+            sound = unlockSound,
+            audio = voiceLog
+        };
+
+        if (!string.IsNullOrEmpty(title)) LanguageHandler.SetLanguageLine("Ency_" + key, title);
+        if (!string.IsNullOrEmpty(desc)) LanguageHandler.SetLanguageLine("EncyDesc_" + key, desc);
+
+        AddEncyclopediaEntry(encyEntryData);
+    }
+}
